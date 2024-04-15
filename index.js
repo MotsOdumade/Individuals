ec5548dd13a22918e02532277cce54e04eab21f7
MotsOdumade <151221439+MotsOdumade@users.noreply.github.com>
2024-04-15 06:33:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,40 +2,42 @@ const express = require('express');
 const http = require('http');
 const https = require('https');
 const fs = require('fs');
-
 const app = express();
-
 // HTTP server
 const httpServer = http.createServer(app);
 const HTTP_PORT = 3001;
 
 
-
-
 // Define your Express routes here
+// ------ handle GET requests -----------------------
+
 app.get('/v1/individual-analytics', (req, res) => {
 
-    // Define the URL for authorisation API
-    const apiCUrl = 'http://localhost:3004/v1/authorisation'; // Assuming API C is running on localhost:3004
+// ------ first request authorisation from authorisation api
+        const apiCUrl = 'http://localhost:3004/v1/authorisation'; // Assuming authorisation api is running on localhost:3004
+        http.get(apiCUrl, (response) => {
+            let data = '';
+            // 'data' will be an array holding the authorisation status
+            response.on('data', (chunk) => { data += chunk; });
+            // The whole response has been received
+            response.on('end', () => {
+                responseObj = {
+                    'cacheable': false,
+                    'authorised': true
+                };
+                console.log('Authorisation Verdict: ', data);
+                res.json(responseObj);
+            });
 
-    // Make a GET request to authorisation API
-    http.get(apiCUrl, (response) => {
-        let data = '';
-        // A chunk of data has been received
-        response.on('data', (chunk) => {
-            data += chunk;
+
+// ------ authorised so can then now carry out the user's request
+            // do something
+            
+// ------ if there was an issue with accessing the authorisation api
+        }).on("error", (error) => {
+                console.error('Error calling authorisation verdict:', error);
+                res.status(500).send('Internal Server Error');
         });
-        // The whole response has been received
-        response.on('end', () => {
-            console.log('Response from Authorisation:', data);
-            // Do something with the response data
-            res.send("GET request on individuals resource " + data);
-        });
-    }).on("error", (error) => {
-        // Handle errors
-        console.error('Error calling API C:', error);
-        res.status(500).send('Internal Server Error');
-    });
 });
 
 httpServer.listen(HTTP_PORT, () => {
