ff7a6fb39ca0afbeade79d18afd5ca9273d742d3
MotsOdumade <151221439+MotsOdumade@users.noreply.github.com>
2024-04-15 02:46:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const express = require('express');
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
 
+const app = express();
+
+// HTTP server
+const httpServer = http.createServer(app);
+const HTTP_PORT = 3001;
+
+// Define your Express routes here
+app.get('/v1/individual-analytics', (req, res) => {
+    res.send("GET request on individuals resource");
+});
+
+httpServer.listen(HTTP_PORT, () => {
+    console.log(`HTTP Server is running on port ${HTTP_PORT}`);
+});
