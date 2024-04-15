9e6876fc7fd42eb135ce8698fd24f26a4e4f270d
MotsOdumade <151221439+MotsOdumade@users.noreply.github.com>
2024-04-15 04:37:43 +0100

    http instead of axios

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 const httpServer = http.createServer(app);
 const HTTP_PORT = 3001;
 
-const axios = require('axios');
+
 
 
 // Define your Express routes here
@@ -19,18 +19,24 @@ app.get('/v1/individual-analytics', (req, res) => {
     const apiCUrl = 'http://localhost:3004/v1/authorisation'; // Assuming API C is running on localhost:3004
 
     // Make a GET request to authorisation API
-    axios.get(apiCUrl)
-      .then(response => {
-        // Handle the response from API C
-        console.log('Response from Authorisation:', response.data);
-        // Do something with the response data
-      })
-      .catch(error => {
+    http.get(apiCUrl, (response) => {
+        let data = '';
+        // A chunk of data has been received
+        response.on('data', (chunk) => {
+            data += chunk;
+        });
+        // The whole response has been received
+        response.on('end', () => {
+            console.log('Response from Authorisation:', data);
+            // Do something with the response data
+            res.send("GET request on individuals resource");
+        });
+    }).on("error", (error) => {
         // Handle errors
         console.error('Error calling API C:', error);
-      });
-        res.send("GET request on individuals resource");
+        res.status(500).send('Internal Server Error');
     });
+});
 
 httpServer.listen(HTTP_PORT, () => {
     console.log(`HTTP Server is running on port ${HTTP_PORT}`);
