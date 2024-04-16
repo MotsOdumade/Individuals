d814f6ee534eed7301f042c5994fc885a657022e
MotsOdumade <151221439+MotsOdumade@users.noreply.github.com>
2024-04-16 07:44:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {authorised } = require('./helpers');
+const {authorised, data_to_chart } = require('./helpers');
 const express = require('express');
 const http = require('http');
 const https = require('https');
@@ -17,15 +17,16 @@ app.get('/v1/individual-analytics', (req, res) => {
 // ------ AUTHORISATION - specified client-token used to verify client's identity and check if they're authorised to access the requested data
 
 // ------ COMPARING
-        // self (time period) - default = now - specified individual-id and time?
-        // average from team - specified project-id 
-        // average employee - specified avg-employee as true
+        // self (time period) - specified data-about = self, target-id = user-id and when = time (default = now)
+        // average from team - specified data-about = project, target-id = project-id
+        // average employee - specified data-about =  avg-employee
 // ------ NUMBER OF PROJECTS - specify data = num-projects
-// ------ PIE CHART - completed / not started / in progress active tasks - specify chart = pie, data = task-status
+// ------ PERFORMANCE REPORT - specify data = performance-report
+// ------ PIE CHART - completed / not started / in progress active tasks - specify chart = pie, data = task-status-proportions
 // ------ PROGRESS BAR - deadlines met in last 7 days - specify chart = progress-bar, data = deadlines-met
-// ------ LINE GRAPH - task weight completion each week specify chart = line, data = weekly-completion
-        res.json({'authorised': authorised(), 'data': location});
-        res.json({'allowed': true});
+// ------ LINE GRAPH - task weight completion each week specify chart = line, data = weekly-task-completion
+        res.json({'authorised': authorised(), 'data': data_to_chart('num-projects')});
+
 });
 
 httpServer.listen(HTTP_PORT, () => {
