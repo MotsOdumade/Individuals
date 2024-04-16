6c94128c6a6d72687b4b20099c75af262b16cd89
MotsOdumade <151221439+MotsOdumade@users.noreply.github.com>
2024-04-16 08:17:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ const HTTP_PORT = 3001;
 app.get('/v1/individual-analytics', (req, res) => {
       
        // Handle params
-        const dataRequested = req.query.data?.trim()?.replace(/<[^>]*>/g, '') || '';
-        const clientToken = req.query['client-token']?.trim()?.replace(/<[^>]*>/g, '') || '';
-        const dataAbout = req.query['data-about']?.trim()?.replace(/<[^>]*>/g, '') || '';
-        const targetId = req.query['target-id']?.trim()?.replace(/<[^>]*>/g, '') || '';
-        const when = req.query.when?.trim()?.replace(/<[^>]*>/g, '') || '';
+        const dataRequested = (req.query.data || '').trim().replace(/<[^>]*>/g, '');
+        const clientToken = (req.query.client-token || '').trim().replace(/<[^>]*>/g, '');
+        const dataAbout = (req.query.data-about || '').trim().replace(/<[^>]*>/g, '');
+        const targetId = (req.query.target-id || '').trim().replace(/<[^>]*>/g, '');
+        const when = (req.query.when || '').trim().replace(/<[^>]*>/g, '');
 
         let location = "undecided";
 // ------ AUTHORISATION - specified client-token used to verify client's identity and check if they're authorised to access the requested data
