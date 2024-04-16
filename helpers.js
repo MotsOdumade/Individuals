f65df3017c19849fb21f90247b199831e14c1b42
MotsOdumade <151221439+MotsOdumade@users.noreply.github.com>
2024-04-16 10:58:40 +0100

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -5,12 +5,14 @@ const dataChartDict = {
       'task-status-proportions': 'pie',
       'performance-report': 'stat',
       'num-projects': 'stat'
-  };
+      // performance-report ought to be broken down further
+};
 
 const listDataAbout = ['self', 'project', 'avg-employee'];
 
 
 function valid_request(data_requested, client_token, data_about, target_id){
+      // check if the request is missing necessary information
   if (Object.keys(dataChartDict).includes(data_requested) === false){
         return false;
   }
@@ -32,13 +34,20 @@ function valid_request(data_requested, client_token, data_about, target_id){
         return false;
   }
   return true;
-  
-
 }
 
-function authorised(){
+
+
+function authorised(client_token, data_about, target_id){
+
+      // match client_token to client id in system - if none matching then return false
+      // if data_about = self then if client id = target_id return true else false
+      // if data_about = avg-employee return true
+      // if data_about = project then if client id is in project with id target_id return true, else false
   return true;
 }
+
+
 
 function data_to_chart(data_requested){
   
