9443e4085d17d2ca248e6f1fded361ee5a4e0b92
MotsOdumade <151221439+MotsOdumade@users.noreply.github.com>
2024-04-16 07:43:20 +0100

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -3,6 +3,22 @@ function authorised(){
   return true;
 }
 
+function data_to_chart(dataRequest){
+  
+  // -- matches dataRequest to a chart type
+  // -- dataToChart = [weekly-task-completion: 'line', deadlines-met: 'progressBar', task-status-proportions: 'pie', performance-report: 'stat', num-projects: 'stat'];
+  
+  dataToChart = [weekly-task-completion: 'line', deadlines-met: 'progressBar', task-status-proportions: 'pie', performance-report: 'stat', num-projects: 'stat'];
+  
+ 
+  if (Object.keys(dataToChart).includes(dataRequest)){
+    // match to the correct chart type
+    const chart = dataToChart[dataRequest];
+    return chart;
+  } else {
+    return false;
+  }
+}
 
 
 
@@ -11,5 +27,4 @@ function authorised(){
 
 
 
-
-module.exports = { authorised};
+module.exports = { authorised, data_to_chart};
