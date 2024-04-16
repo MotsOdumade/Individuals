a1670dff106112ef825db3515ccaeec30a087355
MotsOdumade <151221439+MotsOdumade@users.noreply.github.com>
2024-04-16 10:20:42 +0100

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -1,4 +1,5 @@
-const dataToChart = {
+
+const dataChartDict = {
       'weekly-task-completion': 'line',
       'deadlines-met': 'progressBar',
       'task-status-proportions': 'pie',
@@ -6,8 +7,31 @@ const dataToChart = {
       'num-projects': 'stat'
   };
 
-function valid_request(dataRequested, clientToken, dataAbout, targetId){
-  
+const listDataAbout = ['self', 'project', 'avg-employee'];
+
+
+function valid_request(data_requested, client_token, data_about, target_id){
+  if (Object.keys(dataChartDict).includes(data_requested) === false){
+        return false;
+  }
+  if (client_token == ''){
+        return false;
+  }
+  if (listDataAbout.includes(data_about) === false){
+        return false;
+  }
+  if (data_about == 'avg-employee'){
+        return true;
+  } else {
+        if (target_id = ''){
+              return false;
+        }
+        
+        if (isNaN(target_id)){
+              // it's not a number
+              return false;
+        }
+  }
   return true;
 }
 
@@ -15,20 +39,12 @@ function authorised(){
   return true;
 }
 
-function data_to_chart(dataRequest){
+function data_to_chart(data_requested){
   
   // -- matches dataRequest to a chart type
   // -- dataToChart = [weekly-task-completion: 'line', deadlines-met: 'progressBar', task-status-proportions: 'pie', performance-report: 'stat', num-projects: 'stat'];
-  
-  
- 
-  if (Object.keys(dataToChart).includes(dataRequest)){
-    // match to the correct chart type
-    const chart = dataToChart[dataRequest];
-    return chart;
-  } else {
-    return false;
-  }
+  const chart = dataChartDict[data_requested];
+  return chart;
 }
 
 
