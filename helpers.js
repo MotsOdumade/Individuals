96832598b5805f4f38675b98e0df48f1f14c9819
MotsOdumade <151221439+MotsOdumade@users.noreply.github.com>
2024-05-10 22:21:37 +0100

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -151,7 +151,7 @@ async function num_projects_request(dataAbout, targetId, when){
 }
 async function num_tasks_request(dataAbout, targetId, when){
   const title = 'Number of Active Tasks';
-  let sql_query = `SELECT COUNT(*) AS Tasks FROM task 
+  let sql_query = `SELECT COUNT(*) FROM task 
         LEFT JOIN task_start 
         ON task.id = task_start.task_id
         WHERE task_complete.task_id IS NULL AND deadline > STR_TO_DATE('2024-05-17 13:42:04', '%Y-%m-%d %H:%i:%s') 
