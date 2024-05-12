43303b0af77b795feb5dacdab94fdff22a7d0e01
MotsOdumade <151221439+MotsOdumade@users.noreply.github.com>
2024-05-12 19:03:04 +0100

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -283,10 +283,7 @@ async function member_projects_request(targetId){
   let sampleData;
   // query the database
   let query_all_projects = `SELECT name as 'project-name', id as 'project-id' FROM project;`;
-  let query_projects_in = `SELECT p.id, p.name 
-      FROM project_team_member ptm 
-      JOIN project p ON ptm.project_id = p.id 
-      WHERE ptm.user_id = ${targetId};`;
+  let query_projects_in = `SELECT project_id as id, name from task where assigned_user_id = ${targetId};`;
   let roleQuery = `SELECT COUNT(*) as count FROM user WHERE role LIKE "Manager" AND id = ${targetId};`; 
   let query2;
   try {
